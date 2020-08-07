4ec52ae76f5c50351a3bac460530bdd21de3ca5d
ikhtiar_mahmud <ikhtiar.uddin@bs-23.net>
2020-08-07 22:28:33 +0600

    initial commit

---
+++ code/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docker Fun</title>
+    <style>
+        .congrates {
+            color: #ff5757;
+            text-align: center;
+            margin-top: 20px;
+        }
+        .success {
+            color: green;
+            text-align: center;
+        }
+        .tech {
+            color: orangered;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1 class="congrates">Congratulations! :D</h1>
+    <p class="success">You successfully run your first docker project!</p>
+    <p class="tech">Technology Used : Nginx, PHP-7.4</p>
+</body>
+</html>
